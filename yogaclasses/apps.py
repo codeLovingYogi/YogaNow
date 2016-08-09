26e901b2522bd0f6b39d5359d55e592a59fb1f4f
Johnny <felichay@outlook.com>
2016-08-09 00:08:19 -0400

    Update app name from classes to yogaclasses

---
+++ yogaclasses/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class YogaclassesConfig(AppConfig):
+    name = 'yogaclasses'
--- yogaclasses/views.py
+++ yogaclasses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'classes/index.html', {})
+    return render(request, 'yogaclasses/index.html', {})
 
