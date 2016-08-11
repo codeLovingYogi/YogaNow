72eb979dbf97ba6ba236d7308f6ce5a8d7634882
Johnny <felichay@outlook.com>
2016-08-11 00:11:53 -0400

    Test scraper on another site using mbo and track all details of classes into dicts to iterate

--- scrape.py
+++ scrape.py
@@ -9,7 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 browser = webdriver.PhantomJS()
-browser.get('http://www.stantonstreetyoga.com/classes/schedule/')
+#browser.get('http://www.stantonstreetyoga.com/classes/schedule/')
+browser.get('http://www.jivamuktiyoga.nyc/schedule/')
 
 # wait for page to load schedule data
 wait = WebDriverWait(browser, 20)
@@ -21,20 +22,33 @@ browser.save_screenshot('schedule.png')
 # parse html
 soup = BeautifulSoup(browser.page_source, "html.parser")
 # get scheduling data
-schedule = soup.find('div', {'class': 'mainSL'})
+schedule = soup.find('div', {'class': 'schedule'})
 #print(schedule.prettify())
 
 # get list of classes
+results = []
+
+# find classes based on mbo schedule data organized in odd/even rows
 yoga_classes = soup.find_all('tr', {'class': 'odd'})
+yoga_classes2 = soup.find_all('tr', {'class': 'even'})
 
 # get information about each class
+results = []
 for i in range(len(yoga_classes)):
-	#print(classes[i].prettify())
 	start_time = yoga_classes[i].find('span', {'class': 'hc_starttime'}).get_text()
-	print('start: ', start_time)
+	results.append({'start': start_time})
+	
 	end_time = yoga_classes[i].find('span', {'class': 'hc_endtime'}).get_text()
-	print('end: ', end_time)
+	results[i]['end'] = end_time
+	
 	description = yoga_classes[i].find('span', {'class': 'classname'}).get_text()
-	print('class: ', description)
+	results[i]['description'] = description
+	
 	teacher = yoga_classes[i].find('span', {'class': 'trainer'}).get_text()
-	print('teacher: ', teacher)
+	results[i]['teacher'] = teacher
+	
+print('num results: ', len(results))
+
+for j in range(len(results)):
+	for key, value in results[j].items():
+		print(key, ': ', value)	
