64d4d1308e6257615627cca7683384eb9fa13df4
Johnny <felichay@outlook.com>
2016-08-13 23:07:23 -0400

    Add Studio, Teacher, YogaClass models

--- yogaclasses/models.py
+++ yogaclasses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Studio(models.Model):
+    sname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sname
+
+class Teacher(models.Model):
+    tname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tname
+
+class YogaClass(models.Model):
+    studio = models.ForeignKey(Studio)
+    teacher = models.ForeignKey(Teacher)
+    description = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.description
+
