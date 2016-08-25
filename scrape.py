e91ba61271cb2d48d67fb744adb0d061ed732dff
Johnny <felichay@outlook.com>
2016-08-25 14:54:24 -0400

    Clean up scrape.py by creating YogaClassScraper class and methods

--- scrape.py
+++ scrape.py
@@ -8,46 +8,61 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-def scrape_classes():
-	browser = webdriver.PhantomJS()
-	#browser.get('http://www.stantonstreetyoga.com/classes/schedule/')
-	browser.get('http://www.jivamuktiyoga.nyc/schedule/')
+#link = 'http://www.stantonstreetyoga.com/classes/schedule/'
+link = 'http://www.jivamuktiyoga.nyc/schedule/'
 
-	# wait for page to load schedule data
-	wait = WebDriverWait(browser, 20)
-	wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".healcode")))
-	# set the window size for screenshot
-	browser.set_window_size(1024, 768) 
-	browser.save_screenshot('schedule.png')
 
-	# parse html
-	soup = BeautifulSoup(browser.page_source, "html.parser")
-	# get scheduling data
-	schedule = soup.find('div', {'class': 'schedule'})
-	#print(schedule.prettify())
+class YogaClassScraper(object):
 
-	# get list of classes
-	results = []
+    """Find yoga classes by scraping data from yoga studio schedules."""
+    
+    def __init__(self):
+        self.browser = webdriver.PhantomJS()
+        # set the window size for screenshot
+        self.browser.set_window_size(1024, 768) 
+    
+    def find_classes(self):
+        """Open page, feed HTML into BeautifulSoup, and find classes."""
+        self.browser.get(link)
+        # wait for page to load schedule data
+        wait = WebDriverWait(self.browser, 20)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".healcode")))
+        self.browser.save_screenshot('schedule.png')
+        # store list of classes
+        results = []
+        # parse html
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
+        
+        # get scheduling data
+        #schedule = soup.find('div', {'class': 'schedule'})
+        #print(schedule.prettify())
+        
+        # scrape list of classes based on mbo schedule data organized in odd/even rows
+        yoga_classes = soup.find_all('tr', {'class': 'odd'})
+        yoga_classes2 = soup.find_all('tr', {'class': 'even'})
+        # scrape details about each class
+        self.get_details(yoga_classes, results)
+        self.get_details(yoga_classes2, results)
 
-	# find classes based on mbo schedule data organized in odd/even rows
-	yoga_classes = soup.find_all('tr', {'class': 'odd'})
-	yoga_classes2 = soup.find_all('tr', {'class': 'even'})
+        return results
 
-	# get information about each class
-	def find_classes(classlist, resultlist):
-	    for i in range(len(classlist)):
-	        start_time = classlist[i].find('span', {'class': 'hc_starttime'}).get_text().strip()
-	        time = classlist[i].find('span', {'class': 'hc_endtime'}).get_text().strip()
-	        end_time = re.search('[0-9]+.*', time).group(0)
-	        description = classlist[i].find('span', {'class': 'classname'}).get_text().strip()
-	        teacher = classlist[i].find('span', {'class': 'trainer'}).get_text().strip()
-	        resultlist.append({'start': start_time, 'end': end_time, 'description': description, 'teacher': teacher})
-	    
-	find_classes(yoga_classes, results)
-	find_classes(yoga_classes2, results)
+    def get_details(self, classlist, resultlist):
+        """Get and store details about each yoga class."""
+        for i in range(len(classlist)):
+            start_time = classlist[i].find('span', {'class': 'hc_starttime'}).get_text().strip()
+            time = classlist[i].find('span', {'class': 'hc_endtime'}).get_text().strip()
+            end_time = re.search('[0-9]+.*', time).group(0)
+            description = classlist[i].find('span', {'class': 'classname'}).get_text().strip()
+            teacher = classlist[i].find('span', {'class': 'trainer'}).get_text().strip()
+            resultlist.append({'start': start_time, 'end': end_time, 'description': description, 'teacher': teacher})
 
-	print('num results: ', len(results))
+    def scrape(self):
+        """Main method to start scraper and find yoga classes."""
+        results = self.find_classes()
+        print('num results: ', len(results))
 
-	for j in range(len(results)):
-	    for key, value in results[j].items():
-	        print(key, ': ', value) 
+        for j in range(len(results)):
+            for key, value in results[j].items():
+                print(key, ': ', value) 
+
+        self.browser.quit()
--- yogaclasses/management/commands/scrapeyoga.py
+++ yogaclasses/management/commands/scrapeyoga.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 #from polls.models import Question as Poll
-from scrape import scrape_classes
+from scrape import YogaClassScraper
+
+import datetime
 
 class Command(BaseCommand):
     help = 'Scrapes yoga classes data'
 
     def handle(self, *args, **options):
-    	scrape_classes()
+    	self.stdout.write('\nScraping started at %s\n' % str(datetime.datetime.now()))
+    	s = YogaClassScraper()
+    	s.scrape()
