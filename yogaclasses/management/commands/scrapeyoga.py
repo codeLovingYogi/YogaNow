942ec359a564c966bdbfa5935f1340e13c10aa50
Johnny <felichay@outlook.com>
2016-10-03 17:40:36 -0400

    Connect scraped data to data model and store classes in database

--- yogaclasses/management/commands/scrapeyoga.py
+++ yogaclasses/management/commands/scrapeyoga.py
@@ -13,35 +13,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('\nScraping started at %s\n' % str(datetime.datetime.now()))
         for studio in studios:
+            name = studio['studio']
             link = studio['link']
             s = YogaClassScraper(link)
             results = s.scrape()
-            print('num results: ', len(results))
+            print(name, '- Number of results: ', len(results))
 
-            for j in range(len(results)):
-                for key, value in results[j].items():
-                    print(key, ': ', value) 
+            for yclass in results:
+                studio = name
+                teacher = yclass['teacher']
+                description = yclass['description']
+                class_date = datetime.datetime.now().date()
+                class_start = yclass['start']
+                class_end = yclass['end']
+                # studio = 'Felice Yoga2'
+                # teacher = 'Felice 2'
+                # description = 'Restorative2'
+                # class_date = datetime.date(2016, 8, 28)
+                # class_start = '10:30:00'
+                # class_end = '11:30:00'
+                if not Studio.objects.filter(name=studio):
+                    s = Studio(name=studio)
+                    s.save()
+                s = Studio.objects.get(name=studio)
 
-        studio = 'Felice Yoga2'
-        teacher = 'Felice 2'
-        description = 'Restorative2'
-        class_date = datetime.date(2016, 8, 28)
-        class_start = '10:30:00'
-        class_end = '11:30:00'
-
-        if not Studio.objects.filter(name=studio):
-            s = Studio(name=studio)
-            s.save()
-        s = Studio.objects.get(name=studio)
-
-        if not Teacher.objects.filter(name=teacher):
-            t = Teacher(name=teacher)
-            t.save()
-        t = Teacher.objects.get(name=teacher)
-        
-        if not YogaClass.objects.filter(studio__name=s, teacher__name=t, description=description, date=class_date, start_time=class_start, end_time=class_end):
-            y = YogaClass(studio=s, teacher=t, description=description, date=class_date, start_time=class_start, end_time=class_end)
-            y.save()
-        else: 
-            y = YogaClass.objects.get(studio__name=s, teacher__name=t, description=description, date=class_date, start_time=class_start, end_time=class_end)
-        print(y)
+                if not Teacher.objects.filter(name=teacher):
+                    t = Teacher(name=teacher)
+                    t.save()
+                t = Teacher.objects.get(name=teacher)
+                
+                if not YogaClass.objects.filter(studio__name=s, teacher__name=t, description=description, date=class_date, start_time=class_start, end_time=class_end):
+                    y = YogaClass(studio=s, teacher=t, description=description, date=class_date, start_time=class_start, end_time=class_end)
+                    y.save()
+                else: 
+                    y = YogaClass.objects.get(studio__name=s, teacher__name=t, description=description, date=class_date, start_time=class_start, end_time=class_end)
+                print(y)
