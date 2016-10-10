aae70a49bbfc5b3762d24845bd31cc3932e62c9f
Johnny <felichay@outlook.com>
2016-10-10 14:18:07 -0400

    Add api to url path and clean up url files

--- yoganow/urls.py
+++ yoganow/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from yogaclasses import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('yogaclasses.urls')),
+]
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'studios', views.StudioViewSet)
+router.register(r'teachers', views.TeacherViewSet)
+router.register(r'yogaclasses', views.YogaClassViewSet)
+
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
